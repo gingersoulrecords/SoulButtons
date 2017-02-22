737aa7991b26c0013bc375211e54f496cd069408
Arūnas Liuiza <ask@arunas.co>
2017-02-22 15:51:33 +0200

    account for situations where ga is renamed into __gaTracker

--- SoulButton.js
+++ SoulButton.js
@@ -1,14 +1,14 @@
-// remap ga to __gaTracker
-// __gaTracker( function() {
-//   window.ga = __gaTracker;
-// } );
-
 // initalize tracking of buttons
 jQuery(function($){
 	$('.soulbutton.soulbutton-track').on( 'click', function(e) {
-		if (typeof( ga ) !== 'undefined') {
-			var button =  $(this).attr('data-ga');
-      ga( 'send', 'event', 'button', button );
-		}
+    if ( typeof( ga ) == 'undefined' && typeof( __gaTracker ) == 'undefined' ) {
+      return true;
+    }
+    var tracker_var = 'ga';
+    if ( typeof( __gaTracker ) !== 'undefined' ) {
+      tracker_var = '__gaTracker';
+    }
+		var button =  $(this).attr('data-ga');
+    window[tracker_var]( 'send', 'event', 'button', button );
 	});
 });
